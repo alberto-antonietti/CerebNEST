28036204a5c94a2ae2b36c075c23fe357af63c21
Alberto <alberto.antonietti@polimi.it>
2018-03-19 17:11:25 +0100

    Added basic test: check if all modules can be created/used

---
+++ Tests/Check_Models.py
@@ -0,0 +1,40 @@
+import nest
+
+nest.Install("albertomodule")
+
+VT = nest.Create("volume_transmitter_alberto", 1)
+
+CLOSED = nest.Create("closed_loop_neuron", 1)
+
+RBF = nest.Create("radial_basis_function_input", 1)
+
+PRE = nest.Create("iaf_cond_exp", 1)
+
+POST = nest.Create("iaf_cond_exp", 1)
+
+conn_param1 = {"model":    'stdp_synapse_sinexp',
+               "A_minus": -0.01,   # double - Amplitude of weight change for depression
+               "A_plus":   0.01,   # double - Amplitude of weight change for facilitation 
+               "Wmin":     0.0,    # double - Minimal synaptic weight 
+               "Wmax":     4.0,    # double - Maximal synaptic weight,              
+               "weight":   1.0,
+               "delay":    1.0}
+
+nest.Connect(PRE,POST,{'rule': 'one_to_one'},conn_param1)
+A=nest.GetConnections(PRE,POST)
+nest.SetStatus(A,{'vt': VT[0]})
+
+
+conn_param2 = {"model":    'stdp_synapse_cosexp',
+               "A_minus": -0.01,   # double - Amplitude of weight change for depression
+               "A_plus":   0.01,   # double - Amplitude of weight change for facilitation 
+               "Wmin":     0.0,    # double - Minimal synaptic weight 
+               "Wmax":     4.0,    # double - Maximal synaptic weight,                 
+               "weight":   1.0,
+               "delay":    1.0}
+
+nest.Connect(POST,PRE,{'rule': 'one_to_one'},conn_param2)
+A=nest.GetConnections(POST,PRE)
+nest.SetStatus(A,{'vt': VT[0]})
+
+sys.exit(0) #Everything went fine
