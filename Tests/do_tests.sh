195048d2ab0fa8a7f281ee21cb38972765a54992
alberto-antonietti <alberto.antonietti@polimi.it>
2018-03-19 20:11:54 +0100

    Updated do_tests

--- Tests/do_tests.sh
+++ Tests/do_tests.sh
@@ -7,3 +7,7 @@ mpirun -np 1 python Check_MultiThreading.py 1
 
 mpirun -np 4 python Check_MultiThreading.py 4
 
+mpirun -np 1 python EBCC_closed_loop.py 1
+
+mpirun -np 4 python EBCC_closed_loop.py 4
+
