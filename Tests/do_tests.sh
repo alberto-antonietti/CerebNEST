3c71ea2fa96eeb9001bbd98e3001da9bc5d47aa9
alberto-antonietti <alberto.antonietti@polimi.it>
2019-05-17 10:48:50 +0200

    updated tests

--- Tests/do_tests.sh
+++ Tests/do_tests.sh
@@ -3,6 +3,8 @@ echo "Launching Tests for Alberto Module"
 rm *.csv
 rm *.gdf
 
+#### CHECK MODELS #####
+
 python Check_Models.py  &>TestLog.txt
 if [ $? = 0 ]; then
   echo "Check_Models.py SUCCESS"
@@ -10,13 +12,14 @@ else
   echo "Check_Models.py FAIL"
 fi
 
+#### CHECK MULTITHREADING #####
+
 mpirun -np 1 python Check_MultiThreading.py 1 &>>TestLog.txt
 if [ $? = 0 ]; then
   echo "Check_MultiThreading.py with 1 Core SUCCESS"
 else
   echo "Check_MultiThreading.py with 1 Core FAIL"
 fi
-
 
 mpirun -np 4 python Check_MultiThreading.py 4 &>>TestLog.txt
 if [ $? = 0 ]; then
@@ -34,6 +37,8 @@ if [[ -s Diff_Weights.csv ]]; then
 else
   echo "Weights with 1 and 4 Cores are the same SUCCESS";
 fi
+
+#### CHECK STDP SINEXP #####
 
 mpirun -np 1 python Learning_Performance_Test.py 1 &>>TestLog.txt
 if [ $? = 0 ]; then
@@ -60,6 +65,8 @@ else
   echo "Weights with 1 and 4 Cores are different FAIL"
 fi
 
+#### CHECK STDP COSEXP #####
+
 mpirun -np 1 python Learning_Performance_Test2.py 1 &>>TestLog.txt
 if [ $? = 0 ]; then
   echo "Learning_Performance_Test2.py with 1 core SUCCESS"
@@ -84,6 +91,36 @@ if [ $? = 0 ]; then
 else
   echo "Weights with 1 and 4 Cores are different FAIL"
 fi
+
+#### CHECK iSTDP #####
+
+mpirun -np 1 python Learning_Performance_iSTDP.py 1 &>>TestLog.txt
+if [ $? = 0 ]; then
+  echo "Learning_Performance_iSTDP.py with 1 core SUCCESS"
+else
+  echo "Learning_Performance_iSTDP.py with 1 core FAIL"
+fi
+python Remove_Empty.py &>>TestLog.txt
+mv iSTDP-* iSTDP1.csv
+
+mpirun -np 4 python Learning_Performance_iSTDP.py 4 &>>TestLog.txt
+if [ $? = 0 ]; then
+  echo "Learning_Performance_iSTDP.py with 4 Core SUCCESS"
+else
+  echo "Learning_Performance_iSTDP.py with 4 Core FAIL"
+fi
+python Remove_Empty.py &>>TestLog.txt
+cat iSTDP-* > iSTDP4.csv
+rm iSTDP-*
+
+python Compare_with_Ground_Truth3.py &>>TestLog.txt
+if [ $? = 0 ]; then
+  echo "Weights with 1 and 4 Cores are the same SUCCESS"
+else
+  echo "Weights with 1 and 4 Cores are different FAIL"
+fi
+
+#### CHECK EBCC CLOSED LOOP #####
 
 mpirun -np 1 python EBCC_closed_loop.py 1 &>>TestLog.txt
 if [ $? = 0 ]; then
@@ -112,3 +149,7 @@ if [[ -s Diff_Output.csv ]]; then
 else
   echo "Weights with 1 and 4 Cores are the same SUCCESS";
 fi
+
+
+#### CLEAN-UP THE FOLDER #####
+rm *.csv *.gdf
